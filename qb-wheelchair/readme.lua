e2b373f25b075ddfc067a7b3d8c2b0c4bff4e6d8
Bunny5578 <103382613+Bunny5578@users.noreply.github.com>
2022-05-14 22:11:18 -0400

    Add files via upload

---
+++ qb-wheelchair/readme.lua
@@ -0,0 +1,4 @@
+wheelchair model > https://github.com/AndreIakko/wheelchair
+
+--qb-core/shared/items.lua
+["wheelchair"] 				     = {["name"] = "wheelchair", 			 	 ["label"] = "wheelchair", 		    ["weight"] = 500, 		["type"] = "item", 		["image"] = "wheelchair.png", 				    ["unique"] = true, 		["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Crip Gang"},
