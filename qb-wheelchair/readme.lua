cf76da7b115e643f661c630b0e319951334eaffc
Bunny5578 <103382613+Bunny5578@users.noreply.github.com>
2023-01-08 14:00:31 -0500

    Delete qb-wheelchair directory

--- qb-wheelchair/readme.lua
+++
@@ -1,4 +0,0 @@
-wheelchair model > https://github.com/AndreIakko/wheelchair
-
---qb-core/shared/items.lua
-["wheelchair"] 				     = {["name"] = "wheelchair", 			 	 ["label"] = "wheelchair", 		    ["weight"] = 500, 		["type"] = "item", 		["image"] = "wheelchair.png", 				    ["unique"] = true, 		["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Crip Gang"},
